ec6604555d7e934503640e30e533c6c4dbc27aa4
junbro <khm11904@gmail.com>
2022-10-10 22:55:56 +0900

    style: change board, card

--- src/components/board/board.component.tsx
+++ src/components/board/board.component.tsx
@@ -15,8 +15,10 @@ const Board = ({ boardId }: IBoardProps) => {
     <Wrapper>
       <Title>{boardId}</Title>
       <Droppable droppableId={boardId}>
-        {(boardDroppableProvided) => (
-          <div
+        {(boardDroppableProvided, snapshot) => (
+          <Area
+            isDraggingOver={snapshot.isDraggingOver}
+            isDraggingFromThis={Boolean(snapshot.draggingFromThisWith)}
             ref={boardDroppableProvided.innerRef}
             {...boardDroppableProvided.droppableProps}
           >
@@ -28,7 +30,7 @@ const Board = ({ boardId }: IBoardProps) => {
               />
             ))}
             {boardDroppableProvided.placeholder}
-          </div>
+          </Area>
         )}
       </Droppable>
     </Wrapper>
@@ -36,12 +38,31 @@ const Board = ({ boardId }: IBoardProps) => {
 };
 
 const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
   width: 300px;
-  padding: 20px 10px;
   padding-top: 10px;
   background-color: ${(props) => props.theme.boardColor};
   border-radius: 5px;
   min-height: 300px;
+  overflow: hidden;
+`;
+
+interface IAreaProps {
+  isDraggingOver: boolean;
+  isDraggingFromThis: boolean;
+}
+
+const Area = styled.div<IAreaProps>`
+  flex-grow: 1;
+  padding: 20px;
+  background-color: ${(props) =>
+    props.isDraggingOver
+      ? "#dfe6e9"
+      : props.isDraggingFromThis
+      ? "#b2bec3"
+      : "transparent"};
+  transition: background-color 0.3s ease-in-out;
 `;
 
 const Title = styled.h2`
--- src/components/draggableCard/draggableCard.component.tsx
+++ src/components/draggableCard/draggableCard.component.tsx
@@ -10,8 +10,9 @@ interface IDraggableCardProps {
 const DraggableCard = ({ toDo, index }: IDraggableCardProps) => {
   return (
     <Draggable key={toDo} draggableId={toDo} index={index}>
-      {(cardDraggableProvided) => (
+      {(cardDraggableProvided, snapshot) => (
         <Card
+          isDragging={snapshot.isDragging}
           ref={cardDraggableProvided.innerRef}
           {...cardDraggableProvided.dragHandleProps}
           {...cardDraggableProvided.draggableProps}
@@ -23,11 +24,14 @@ const DraggableCard = ({ toDo, index }: IDraggableCardProps) => {
   );
 };
 
-const Card = styled.div`
+const Card = styled.div<{ isDragging: boolean }>`
   border-radius: 5px;
   margin-bottom: 5px;
-  padding: 10px 10px;
-  background-color: ${(props) => props.theme.cardColor};
+  padding: 10px;
+  background-color: ${(props) =>
+    props.isDragging ? "#e4f2ff" : props.theme.cardColor};
+  box-shadow: ${(props) =>
+    props.isDragging ? "0px 2px 5px rgba(0, 0, 0, 0.05)" : "none"};
 `;
 
 export default React.memo(DraggableCard);
